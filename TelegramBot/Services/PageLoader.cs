73e955128cb85f80bfd9871739745ff1eb0bd7cf
Egor1712 <58880149+Egor1712@users.noreply.github.com>
2021-01-15 18:35:44 +0500

    Initial commit

---
+++ TelegramBot/Services/PageLoader.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+
+namespace Services
+{
+    public class PageLoader
+    {
+        private readonly string rootUrl;
+        private readonly IBrowsingContext context;
+        private readonly string userName;
+        private readonly string password;
+        public IHtmlAllCollection HtmlContent => context.Active.All;
+
+        public PageLoader(string rootUrl, string userName, string password)
+        {
+            this.rootUrl = rootUrl;
+            this.userName = userName;
+            this.password = password;
+            var config = Configuration.Default
+                                      .WithDefaultCookies()
+                                      .WithDefaultLoader();
+            context = BrowsingContext.New(config);
+        }
+
+        public async Task Authorize()
+        {
+            await context.OpenAsync(rootUrl);
+            var login =
+                context.Active
+                       .QuerySelector<IHtmlInputElement>("input#userNameInput");
+            login.Value = userName;
+            var passwordInput =
+                context.Active
+                       .QuerySelector<IHtmlInputElement>("input#passwordInput");
+            passwordInput.Value = password;
+            var button = context.Active.QuerySelector<IHtmlFormElement>("form");
+            await button.SubmitAsync();
+        }
+
+        public async void GoToPageAsync(string newUrl)
+        {
+            if (!newUrl.StartsWith(rootUrl))
+                return;
+            await context.OpenAsync(newUrl);
+        }
+
+        public string GetContentOfElement<T>(string id, string elementType)
+            where T : class, IElement
+        {
+            var element = context.Active.QuerySelector<T>($"{elementType}#{id}");
+            if (element is null)
+                throw new Exception($"Element with id: \"{id}\" was not found");
+            return element.NodeValue;
+        }
+
+        public IEnumerable<T> GetAllElements<T>(string type, string className)
+            where T : class, IElement
+        {
+            return context.Active.QuerySelectorAll<T>(type)
+                          .Where(x => x.ClassName != null && x.ClassName.Contains(className));
+        }
+    }
+}
---
+++ TelegramBot/Services/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+
+namespace Services
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var url = "https://istudent.urfu.ru/s/http-urfu-ru-ru-students-study-brs/";
+            var userName = "chusoveg17@gmail.com";
+            var passsword = "6E2tjFtp";
+            var pageLoader = new PageLoader(url ,userName, passsword );
+            await pageLoader.Authorize();
+            var subjects = pageLoader
+                           .GetAllElements<IHtmlAnchorElement>("a", "js-service-rating-link")
+                           .ToList();
+            var rates = pageLoader
+                        .GetAllElements<IHtmlTableDataCellElement>("td", "js-service-rating-td td-1")
+                        .ToList();
+            for (var i = 0; i < subjects.Count; i++)
+                Console.WriteLine($"{subjects[i].Text} ---- {rates[i].TextContent}");
+        }
+    }
+}
