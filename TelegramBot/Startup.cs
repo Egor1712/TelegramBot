086e03e045960b3bc1ccd06a686cfa2712686177
Egor1712 <58880149+Egor1712@users.noreply.github.com>
2021-01-19 16:49:12 +0500

    commit

--- TelegramBot/Startup.cs
+++ TelegramBot/Startup.cs
@@ -1,9 +1,10 @@
-
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.AzureADB2C.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -11,19 +12,10 @@ namespace TelegramBot
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(AzureADB2CDefaults.AuthenticationScheme)
-                    .AddAzureADB2C(options => Configuration.Bind("AzureAdB2C", options));
-            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,26 +25,17 @@ namespace TelegramBot
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseRouting();
 
             app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
                              {
-                                 endpoints.MapRazorPages();
-                                 endpoints.MapControllers();
+                                 endpoints.MapGet("/",
+                                                  async context =>
+                                                  {
+                                                      await context.Response
+                                                          .WriteAsync("Hello World!");
+                                                  });
                              });
         }
     }
