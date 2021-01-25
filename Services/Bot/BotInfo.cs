0fb95f7f00995a9ac96829e6ad3cfcf1eb839c20
Egor1712 <58880149+Egor1712@users.noreply.github.com>
2021-01-25 12:48:33 +0500

    Update BotInfo.cs

--- Services/Bot/BotInfo.cs
+++ Services/Bot/BotInfo.cs
@@ -3,6 +3,6 @@
     public static class BotInfo
     {
         public const string BotToken = "1473985800:AAEh69_dnPg3-ef0ItLpaIx9lQDtINQwIQs";
-        public const string Url  = "https://52c0d703c56e.ngrok.io/Message/Post";
+        public const string Url  = "https://telegrambot34.herokuapp.com//Message/Post";
     }
 }
